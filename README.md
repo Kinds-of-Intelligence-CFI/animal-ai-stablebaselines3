3d9e17e7fcd783eafe112eb5168222064456ecd9
Wout Schellaert <wout.schellaert@gmail.com>
2023-12-12 17:48:56 +0100

    Setup training

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # AnimalAI + Stable Baselines 3
 
+A template repository for integrating the AnimalAI environment with Stable Baselines 3 agents.
+
+This project was configured using [Rye](https://rye-up.com/), but it should work with any tool supporting pyproject.toml.
+
+**Note: Check the version requirements, they are quite particular.**  
+**Note: Confirmed working with python == 3.9**  
+**Note: Very modern GPUs might not be compatible because of the older Torch version.**
+
+## Usage
+
+- Playing `rye run play`
+- Training `rye run train`
+
+Check the [project.scripts] section of pyproject.toml for the relevant configuration.
+
 ## Resources
 
 - [Docs](https://stable-baselines3.readthedocs.io/en/v2.0.0/) for Stable Baselines 3 ==v2.0.0.
--- src/animalai_stable_baselines/train.py
+++ src/animalai_stable_baselines/train.py
@@ -7,15 +7,40 @@ logging.basicConfig(
     format="[%(asctime)s] [%(levelname)-8s] [%(module)s] %(message)s",
     level=logging.INFO,
 )
-from gym.wrappers.compatibility import EnvCompatibility
-from animalai.envs.environment import AnimalAIEnvironment
-from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
+import animalai
+import animalai.envs.environment
+import mlagents_envs
+import mlagents_envs.envs.unity_gym_env
+import stable_baselines3
 
 import animalai_stable_baselines.utils as utils
 
 
 def main():
-    env = AnimalAIEnvironment(
+    # Create an AnimalAI environment
+    env = animalai.envs.environment.AnimalAIEnvironment(
         file_name=str(utils.find_env_path(Path("./aai/"))),
+        arenas_configurations=str("./aai/configs/sanityGreen.yml"),
+        resolution=64,
+        useCamera=True,
+        useRayCasts=False,
+        no_graphics=False,
     )
-    raise NotImplementedError("TODO: Implement this")
+    # Make it compatible with legacy Gym v0.21 API
+    env = mlagents_envs.envs.unity_gym_env.UnityToGymWrapper(
+        env,
+        uint8_visual=True,
+        # allow_multiple_obs=True,  # Also provide health, velocity (x, y, z), and global position (x, y, z)
+        flatten_branched=True,  # Necessary if the agent doesn't support MultiDiscrete action space.
+    )
+
+    print("Starting training...")
+    # Stable Baselines3 A2C model
+    # Will automatically use Shimmy to convert the legacy Gym v0.21 API to the Gymnasium API
+    model = stable_baselines3.A2C(
+        "MlpPolicy",
+        env,  # type: ignore
+        device="cpu",
+        verbose=1,
+    )
+    model.learn(total_timesteps=10_000)
